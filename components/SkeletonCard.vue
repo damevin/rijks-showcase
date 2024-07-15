e9eb766310cd9e42932feea9de81f98728f4b420
damevin <r.damevin@lehibou.com>
2024-07-15 23:38:30 +0400

    refactor: :recycle: update default height, add variable color

--- components/SkeletonCard.vue
+++ components/SkeletonCard.vue
@@ -10,7 +10,7 @@
 .skeleton {
   width: 100%;
   border-radius: 8px;
-  height: 100%;
-  animation: pulse infinite 1.2s ease-in-out;
+  background-color: var(--color-skeleton);
+  height: 500px;
 }
 </style>
