be92b985acd7d41afad44d4a46b986b86dbf3450
damevin <r.damevin@lehibou.com>
2024-07-15 23:40:14 +0400

    feat: :sparkles: create no results page

--- components/CollectionNoResults.vue
+++ components/CollectionNoResults.vue
@@ -1,37 +1,56 @@
 <script lang="ts" setup>
+const emit = defineEmits<{
+  (e: 'update:query', query: string): void
+}>()
 
+const suggestedQueries = ['Rembrandt van Rijn', 'Johannes Vermeer', 'Frans Hals', 'Vincent van Gogh']
 </script>
 
 <template>
   <div class="container">
     <div>
-      <h1 class="title">
-        <span class="round">
-          ❋
-        </span>
-        No results found
-      </h1>
-      <span>Try something different...</span>
+      <div class="wrapper">
+        <h1 class="container__title">
+          <span class="round">
+            ❋
+          </span>
+          No results found
+        </h1>
+        <span>Try something different...</span>
+      </div>
+      <div v-for="query in suggestedQueries" :key="query">
+        <UiBtn variant="plain" class="query__btn" @click="emit('update:query', query)">
+          {{ query }}
+        </UiBtn>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.container {
-
-}
-
 .wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
 }
 
-.title {
-  font-size: 50px;
-  color: #d55140
+@media screen and (min-width: 768px) {
+  .wrapper {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+}
+
+.query__btn {
+  margin: 0;
+  font-size: clamp(1rem, 10vw, 8rem);
 
 }
 
-.round:hover {
-  animation: spin 2s linear infinite;
+.container__title {
+  font-size: clamp(1rem, 10vw, 2.5rem);
+  color: var(--primary-color);
 }
 </style>
