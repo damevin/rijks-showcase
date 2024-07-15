d5ed55375ce90631f0715b3d668b2ede5c9af2dd
damevin <r.damevin@lehibou.com>
2024-07-15 23:43:25 +0400

    feat: :mag: remove useless code, set seo meta, improve skeletons

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import type { ArtObject } from '~/types/collections.types'
-
 const {
   updateSearchQuery,
   collections,
@@ -13,22 +11,9 @@ const {
   error,
 } = useCollections()
 
-const { toggleCollection } = useCollectionsStore()
-
-const selectedCollection = ref<ArtObject | null>(null)
-
-const modalState = reactive({
-  collectionView: false,
-})
-
-function handleArtObjectDetailledView(collection: ArtObject) {
-/*   console.log('collection', collection) */
-  selectedCollection.value = collection
-  modalState.collectionView = true
-}
-
-const query = ref('')
+const query = ref<string>('')
 const searchInputPlaceholder = 'Search for a collection'
+
 const displayMoreText = computed(() => {
   if (isFetchingNextPage.value)
     return 'Loading'
@@ -36,15 +21,22 @@ const displayMoreText = computed(() => {
     return 'Load more'
   return 'No more results'
 })
-const displayMoreIsDisabled = computed(() => {
-  return Boolean(!hasNextPage || isFetchingNextPage)
+
+function handleUpdateSearchQuery(newQuery: string) {
+  query.value = newQuery
+  updateSearchQuery(newQuery)
+}
+
+useSeoMeta({
+  title: 'Rijks - Collections',
+  description: 'Discover the best collections from the Rijks museum',
 })
 </script>
 
 <template>
   <div class="main">
     <header class="main__header">
-      <UiInputText
+      <UiSearchbar
         v-model="query"
         :loading="isLoading"
         :placeholder="searchInputPlaceholder"
@@ -53,54 +45,59 @@ const displayMoreIsDisabled = computed(() => {
         <template #button>
           <UiBtn
             variant="fab"
+            aria-label="Search for a collection"
             @click="updateSearchQuery(query)"
           >
             <Icon v-if="!isLoading" name="ic:outline-search" />
             <Icon v-else name="svg-spinners:wind-toy" />
           </UiBtn>
         </template>
-      </UiInputText>
+      </UiSearchbar>
     </header>
     <div v-if="isError">
       An error occurs : {{ error }}
     </div>
     <div v-if="!isError && !isLoading && !collections?.length">
-      <CollectionNoResults />
+      <CollectionNoResults @update:query="handleUpdateSearchQuery" />
     </div>
     <div>
-      <CollectionGrid>
+      <CollectionGrid v-if="!isLoading">
         <div v-for="(collection, index) in collections" :key="collection?.id">
-          <CollectionCard
+          <LazyCollectionCard
+            v-if="collection"
             :collection="collection!"
             :style="{ animationDelay: `${index * 0.1}s` }"
             @click="navigateTo(`/collection-${collection?.objectNumber}`)"
-            @update:saved-collection="toggleCollection(collection!)"
           />
         </div>
-        <template v-if="isLoading || isFetching">
-          <div v-for="i in 20" :key="i">
+        <template v-if="isFetchingNextPage">
+          <div v-for="i in 10" :key="`skeleton-${i}`" aria-hidden="true">
             <SkeletonCard />
           </div>
         </template>
       </CollectionGrid>
+
+      <template v-if="isLoading || (isFetching && !collections?.length)">
+        <CollectionGrid aria-hidden="true">
+          <div v-for="i in 20" :key="`initial-skeleton-${i}`">
+            <SkeletonCard />
+          </div>
+        </CollectionGrid>
+      </template>
       <div v-if="collections?.length" class="main__footer">
         <UiBtn
-          :disabled="displayMoreIsDisabled"
+          v-if="hasNextPage && !isFetchingNextPage"
           :loading="isFetchingNextPage"
           variant="secondary"
+          aria-label="Load more collections"
           @click="loadMore"
         >
           {{ displayMoreText }}
           <Icon v-if="isFetching" name="svg-spinners:wind-toy" />
         </UiBtn>
+        <span v-else>No more results</span>
       </div>
     </div>
-    <UiModal v-model="modalState.collectionView">
-      {{ selectedCollection }}
-      <div>
-        <ZoomableImg :src="selectedCollection?.webImage?.url" />
-      </div>
-    </UiModal>
   </div>
 </template>
 
