8656bd0ada922f48ae17559b627db0035f780775
damevin <r.damevin@lehibou.com>
2024-07-15 23:39:18 +0400

    feat: :sparkles: remove masonry variant, improve style

--- components/CollectionGrid.vue
+++ components/CollectionGrid.vue
@@ -1,42 +1,23 @@
-<script lang="ts" setup>
-withDefaults(
-  defineProps<{
-    variant?: 'grid' | 'masonry'
-  }>(),
-  {
-    variant: 'grid',
-  },
-)
-</script>
-
 <template>
-  <ClientOnly>
-    <div
-      :class="{
-        'collection-grid': variant === 'grid',
-        'masonry': variant === 'masonry',
-      }"
-    >
-      <slot />
-    </div>
-  </ClientOnly>
+  <div
+    class="collection-grid"
+  >
+    <slot />
+  </div>
 </template>
 
 <style scoped>
 .collection-grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  display: flex;
+  flex-direction: column;
   gap: 1rem;
 }
 
-.masonry {
-  column-count: 3;
-  column-gap: 1rem;
-
-}
-
-.masonry > * {
-  break-inside: avoid;
-  
+@media screen and (min-width: 768px) {
+  .collection-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+    gap: 1rem;
+  }
 }
 </style>
